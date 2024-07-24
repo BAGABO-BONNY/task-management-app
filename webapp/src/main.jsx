441ec7a732b09eb2ee90a992f4fdc01d95ecf48a
BAGABO-BONNY <bagabobonny544@gmail.com>
2024-07-24 16:49:33 +0200

    f

--- webapp/src/main.jsx
+++ webapp/src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import ReactModal from "react-modal";
 
 import App from "./App.jsx";
-import "./index.css";
+import "./indexs.css";
 
 ReactModal.setAppElement("#root");
 
